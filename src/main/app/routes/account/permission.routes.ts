04728accdb13ff117b289028c9ca798bd4ee42f0
Israel dos Santos Perreira Batista <ibatista@INFORMS>
2021-06-02 14:45:37 -0300

    protected routes middleware

--- src/main/app/routes/account/permission.routes.ts
+++ src/main/app/routes/account/permission.routes.ts
@@ -4,12 +4,11 @@ import { adapterRoute } from "../../adapters/express/expressRouteAdapter";
 import { makeCreatePermisssionController } from "../../factories/controller/account/permissionController";
 import { auth } from "../../middlewares/auth";
 import { can } from "../../middlewares/can";
-import { is } from "../../middlewares/is";
 
 const permissionRouter = Router();
 
 const createPermissionController = makeCreatePermisssionController();
 
-permissionRouter.post("/", can, adapterRoute(createPermissionController));
+permissionRouter.post("/", auth, can, adapterRoute(createPermissionController));
 
 export { permissionRouter };
--- src/main/app/routes/account/users.routes.ts
+++ src/main/app/routes/account/users.routes.ts
@@ -2,11 +2,12 @@ import { Router } from "express";
 
 import { adapterRoute } from "../../adapters/express/expressRouteAdapter";
 import { makeCreateUserController } from "../../factories/controller/account/userController";
+import { is } from "../../middlewares/is";
 
 const usersRouter = Router();
 
 const createUserController = makeCreateUserController();
 
-usersRouter.post("/", adapterRoute(createUserController));
+usersRouter.post("/", is, adapterRoute(createUserController));
 
 export { usersRouter };
